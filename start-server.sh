6effe7cc6c261a8bac1d2b2ab1f8344c89e54a0a
mhskibili <chan_meng_hin@students.edu.sg>
2026-09-10 17:46:38 +0800

    Modify start-server.sh to use bash and start HTTP server
    Updated the script to use bash, added comments, and changed the command to start an HTTP server.

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
-node server.js
+#!/bin/bash
+# Start HTTP server and keep it running
+cd /workspaces/Html-Viewer
+echo "Starting HTTP server on port 8080..."
+exec http-server -p 8080 -a 0.0.0.0 -c-1 --cors
